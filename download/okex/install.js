1b2fdc891695cab12648e151f48e943e369204a1
金小白 <>
2019-02-20 17:43:25 +0800

    更新 okex 下载页面

---
+++ download/okex/install.js
@@ -0,0 +1,1 @@
+jQuery("#install").click(function(){-1<navigator.userAgent.indexOf("iPhone")||-1<navigator.userAgent.indexOf("iOS")?window.location.href="itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-manifest.plist":window.location.href="http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKEx-android.apk"});
