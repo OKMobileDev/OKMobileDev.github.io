7693d6b74f149ddb37fc375f0ca91e79a9c2de60
金小白 <>
2019-02-21 10:27:50 +0800

    添加越南站

---
+++ download/okex-vn/install.js
@@ -0,0 +1,4 @@
+document.getElementById('install').onclick = function () {
+    alert('asdasdasd');
+    -1 < navigator.userAgent.indexOf("iPhone") || -1 < navigator.userAgent.indexOf("iOS") ? window.location.href = "https://itunes.apple.com/app/id1327268470" : window.location.href = "https://play.google.com/store/apps/details?id=com.okinc.okex"
+}
