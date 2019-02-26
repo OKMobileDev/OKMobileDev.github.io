1713298d19f62b4490e786752393992fedee19e9
金小白 <>
2019-02-26 17:17:54 +0800

    优化代码

--- download/okcoin/install.js
+++ download/okcoin/install.js
@@ -4,9 +4,9 @@ document.getElementById('install').onclick = function () {
     let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
     if (isiOS) {
         if (-1 < window.location.toString().indexOf("ios.html")) {
-            window.location.href = "itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-manifest.plist"
+            window.location.href = "itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-manifest.plist";
         } else {
-            window.location = 'ios.html'
+            window.location = 'ios.html';
         }
     } else {
         window.location.href = "http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKEx-android.apk";
