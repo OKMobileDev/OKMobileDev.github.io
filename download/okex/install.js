17b0ee2a903af439facc452b20959ce16e423f6c
金小白 <>
2019-03-04 11:26:07 +0800

    添加下载按钮点击统计

--- download/okex/install.js
+++ download/okex/install.js
@@ -1,3 +1,11 @@
 document.getElementById('install').onclick = function () {
-    -1 < navigator.userAgent.indexOf("iPhone") || -1 < navigator.userAgent.indexOf("iOS") ? window.location.href = "itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-manifest.plist" : window.location.href = "http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKEx-android.apk"
+    let u = navigator.userAgent;
+    let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    if (isiOS) {
+        window.location.href = "itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-manifest.plist";
+        _czc.push(["_trackEvent", "下载", "点击", "okex_ios"]);
+    } else {
+        window.location.href = "http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKEx-android.apk";
+        _czc.push(["_trackEvent", "下载", "点击", "okex_android"]);
+    }
 }
