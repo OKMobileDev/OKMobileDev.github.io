668853a3e855c1286419e5de55ccb80b1f5b4a38
金小白 <>
2019-03-05 20:28:25 +0800

    更新 OKCoin 下载地址

--- download/okcoin/install.js
+++ download/okcoin/install.js
@@ -4,13 +4,13 @@ document.getElementById('install').onclick = function () {
     let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
     if (isiOS) {
         if (-1 < window.location.toString().indexOf("ios.html")) {
-            window.location.href = "itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-manifest.plist";
+            window.location.href = "itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-okcoin-manifest.plist";
             _czc.push(["_trackEvent", "下载", "okcoin_ios_install_click"]);
         } else {
             window.location = 'ios.html';
         }
     } else {
-        window.location.href = "http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKEx-android.apk";
+        window.location.href = "http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKCoin-android.apk";
         _czc.push(["_trackEvent", "下载", "okcoin_android_install_click"]);
     }
 }
