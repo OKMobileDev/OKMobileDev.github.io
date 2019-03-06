82a4a5852033a66c6cf741ce6088cc34a69e72f4
金小白 <>
2019-03-06 11:40:08 +0800

    OKCoin 添加下载引导

--- download/okcoin/install.js
+++ download/okcoin/install.js
@@ -1,4 +1,35 @@
-document.getElementById('install').onclick = function () {
+/**
+ * 按钮点击事件
+ */
+$(document).ready(function(){
+    $(".btn").click(function(){
+        var isWx = isWeChat();
+        if (isWx) {
+            $(".wx-tip").show();   
+        } else {
+            downloadAction();
+        }
+    });
+});
+
+/**
+ * 判断是否是微信
+ */
+function isWeChat(){
+	var ua = window.navigator.userAgent.toLowerCase();
+	if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+		return true;
+    }
+    if (ua.match(/MicroMessenger/i) == 'wxwork') {
+        return true;
+    }
+	return false;
+};
+
+/**
+ * 下载事件
+ */
+function downloadAction() {
     let u = navigator.userAgent;
     let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
     let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
@@ -12,5 +43,5 @@ document.getElementById('install').onclick = function () {
     } else {
         window.location.href = "http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKCoin-android.apk";
         _czc.push(["_trackEvent", "下载", "okcoin_android_install_click"]);
-    }
+    }   
 }
