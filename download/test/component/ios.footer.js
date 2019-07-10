02cccabe308923a7ba3d5e6ff9e69d260e587e35
金小白 <>
2019-07-10 11:52:50 +0800

    iOS 添加推荐角标

--- download/test/component/ios.footer.js
+++ download/test/component/ios.footer.js
@@ -3,16 +3,14 @@ Vue.component('ios-install-footer', {
         return {
             installAppstore: 'App Store',
             installBeta: '安装内测版',
-            appstoreLoading: false,
-            betaLoading: false,
         }
     },
     methods: {
         handleAppstoreDownload () {
-            this.appstoreLoading = true;
+            
         },
         handleBetaDownload () {
-            this.betaLoading = true;
+            
         }
     },
     template: `
@@ -20,7 +18,10 @@ Vue.component('ios-install-footer', {
         <div style="display: flex;width: 100%; height:100%;">
             <div style="position: flex; width: 50%;">
                 <div style="display: flex;">
-                    <el-button type="primary" style="width: 150px; height: 40px; margin-left:auto; margin-right: auto; margin-top: 17px; font-size: 14px; background-color: #333333; border-width: 0px" :loading="appstoreLoading" @click="handleAppstoreDownload"><i class="icon iconfont icon-el-icon-apple""></i>&nbsp&nbsp{{ installAppstore }}</el-button>
+                    <el-button type="primary" style="width: 80%; height: 40px; margin-left:auto; margin-right: auto; margin-top: 17px; font-size: 14px; background-color: #333333; border-width: 0px" @click="handleAppstoreDownload">
+                        <i class="icon iconfont icon-el-icon-apple""></i>&nbsp&nbsp{{ installAppstore }}
+                    </el-button>
+                    <img style="position: absolute; margin-top: 8px; left: 42%; width: 24px; height: 24px;" src="../v2/icon/icon-ios-recommend.png"/>
                 </div>
                 <div>
                     <div style="margin-top: 16px; text-align: center; font-size: 14px; font-weight: 900; color: #333333;">稳定安全，更新方便快捷</div>
@@ -29,7 +30,7 @@ Vue.component('ios-install-footer', {
             </div>
             <div style="position: flex; width: 50%;">
                 <div style="display: flex;">
-                    <el-button type="primary" style="width: 150px; height: 40px; margin-left:auto; margin-right: auto; margin-top: 17px; font-size: 14px; background-color: #ffffff; color: #000; border-width: 1px; border-color: #000;" :loading="betaLoading" @click="handleBetaDownload">{{ installBeta }}</el-button>
+                    <el-button type="primary" style="width: 80%; height: 40px; margin-left:auto; margin-right: auto; margin-top: 17px; font-size: 14px; background-color: #ffffff; color: #000; border-width: 1px; border-color: #000;" @click="handleBetaDownload">{{ installBeta }}</el-button>
                 </div>
                 <div>
                     <div style="margin-top: 16px; text-align: center; font-size: 14px; font-weight: 900; color: #333333;">安装简便快捷</div>
