05b10ab6d7a15d6fd5ed3a4dbf7d554e3b698da5
金小白 <>
2019-07-10 12:25:39 +0800

    图片整理

--- download/test/index.js
+++ download/test/index.js
@@ -7,19 +7,19 @@ new Vue({
             headerSubtitle: '安全 · 稳定 · 可信',
             logo: '../test/images/logo.png',
             tipsData: [{
-                icon: '../v2/icon/icon_security.png',
+                icon: '../v2/icon/icon-security.png',
                 title: '更安全',
                 subtitle: '老牌交易所',
             }, {
-                icon: '../v2/icon/icon_candy.png',
+                icon: '../v2/icon/icon-candy.png',
                 title: '更多糖果',
                 subtitle: '多元化奖励机制',
             }, {
-                icon: '../v2/icon/icon_various.png',
+                icon: '../v2/icon/icon-various.png',
                 title: '更多样',
                 subtitle: '支持合约等多种交易类型',
             }, {
-                icon: '../v2/icon/icon_science.png',
+                icon: '../v2/icon/icon-science.png',
                 title: '更科学',
                 subtitle: '支持ETT组合交易，告别行情过山车',
             }]
