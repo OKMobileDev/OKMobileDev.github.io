f456198eec5b19fa8b902314c823ed1e23d5ca1a
金小白 <>
2019-07-12 18:48:14 +0800

    store 添加展示条件

--- download/v2/component/ios.store.footer.js
+++ download/v2/component/ios.store.footer.js
@@ -1,5 +1,5 @@
 Vue.component('ios-store-install-footer', {
-    props: ['title'],
+    props: ['title', 'black'],
     data: function () {
         return {
             loading: false,
@@ -17,7 +17,12 @@ Vue.component('ios-store-install-footer', {
     template: `
     <div style="position: fixed; background-color: white; bottom: 0; height: 80px; width: 100%; max-width: 500px; z-index: 999; box-shadow:0px 2px 4px 0px rgba(0,0,0,0.5);">
         <el-row type="flex" style="margin-top: 19px; margin-bottom: 19px;" justify="center" align="middle">
-            <el-button type="primary" style="width: 280px; font-size: 14px; background-color: #3075EE; border-width: 0px" :loading="loading" @click="handleDownload"><i class="icon iconfont icon-el-icon-apple""></i>&nbsp&nbsp{{ title }}</el-button>
+            <template v-if="black">
+                <el-button type="primary" style="width: 280px; font-size: 14px; background-color: #333; border-width: 0px" :loading="loading" @click="handleDownload"><i class="icon iconfont icon-el-icon-apple""></i>&nbsp&nbsp{{ title }}</el-button>
+            </template>
+            <template v-else>
+                <el-button type="primary" style="width: 280px; font-size: 14px; background-color: #3075EE; border-width: 0px" :loading="loading" @click="handleDownload"><i class="icon iconfont icon-el-icon-apple""></i>&nbsp&nbsp{{ title }}</el-button>
+            </template>
         </el-row>
     </div>
     `
