699ec1001502e39c3fc0c7774920fa76320042f5
金小白 <>
2019-07-12 12:27:09 +0800

    添加账号列表

--- download/test/ios.store.js
+++ download/test/ios.store.js
@@ -4,7 +4,11 @@ new Vue({
         return {
             logo: '../test/images/logo-ios-store.png',
             headerTitle: 'OKEx App Store 安装',
-            headerSubtitle: '如果您拥有非中国地区的Apple ID，请直接使用该ID登录App Store，下载OKEx APP即可。',
+            footerTitle: '我已知晓 去 App store 安装',
+            formTitle: '输入非中国大陆苹果账号',
+            formSubtitle1: '以下美国 ID 供参考使用，请记录好您所使用的账号及密码，以便更新时使用',
+            formSubtitle2: '为防止您的手机信息泄露，请务必不要使用上方 Apple ID 登录 iCloud。使用以上 Apple ID 下载过后，请您及时退出该账号',
+            headerSubtitle: '如果您拥有非中国地区的 Apple ID，请直接使用该 ID 登录 App Store，下载 OKEx APP 即可。',
             tipsData1: [
                 {
                     title: '进入 App Store 应用',
@@ -29,11 +33,67 @@ new Vue({
                     img: './images/ios-store-tips-2-2.png',
                 }
             ],
+            tableData: [
+                {
+                    account: '账号',
+                    password: '密码'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+                {
+                    account: 'OKEX_bj@163.com',
+                    password: 'Ok123456'
+                },
+            ]
         }
     },
     methods: {
         handleDownload() {
             window.location.href = 'https://itunes.apple.com/us/app/okex-bitcoin-cryptocurrency/id1327268470?mt=8';
         },
+        rowStyle({ row, rowIndex}) {
+            if (rowIndex == 0) {
+                return 'background-color: rgba(48,117,238,0.05); color: #999; font-size: 12px; height: 30px;'
+            }else {
+                return 'background-color: rgba(48,117,238,0.05); font-size: 12px; height: 30px;'
+            }
+        },
+        cellStyle() {
+            return 'padding: 0';
+        }
     }
 });
--- download/v2/component/ios.store.footer.js
+++ download/v2/component/ios.store.footer.js
@@ -1,14 +1,14 @@
 Vue.component('ios-store-install-footer', {
+    props: ['title'],
     data: function () {
         return {
-            install: '我已知晓 去App store安装',
             loading: false,
         }
     },
     methods: {
         handleDownload() {
             if (isWeChat() == false) {
-                this.install = '正在安装...';
+                this.title = '正在安装...';
                 this.loading = true;
             }
             this.$emit('store-click');
@@ -17,7 +17,7 @@ Vue.component('ios-store-install-footer', {
     template: `
     <div style="position: fixed; background-color: white; bottom: 0; height: 80px; width: 100%; max-width: 500px; z-index: 999; box-shadow:0px 2px 4px 0px rgba(0,0,0,0.5);">
         <el-row type="flex" style="margin-top: 19px; margin-bottom: 19px;" justify="center" align="middle">
-            <el-button type="primary" style="width: 280px; font-size: 14px; background-color: #3075EE; border-width: 0px" :loading="loading" @click="handleDownload"><i class="icon iconfont icon-el-icon-apple""></i>&nbsp&nbsp{{ install }}</el-button>
+            <el-button type="primary" style="width: 280px; font-size: 14px; background-color: #3075EE; border-width: 0px" :loading="loading" @click="handleDownload"><i class="icon iconfont icon-el-icon-apple""></i>&nbsp&nbsp{{ title }}</el-button>
         </el-row>
     </div>
     `
