b93ce3b121dbfd2c0b8b5cc02b38a5176a7735b5
金小白 <>
2019-07-12 18:20:18 +0800

    删除无用文件

--- download/test/install.js
+++
@@ -1,3 +0,0 @@
-$(".button").click(function () {
-    -1 < navigator.userAgent.indexOf("iPhone") || -1 < navigator.userAgent.indexOf("iOS") ? window.location.href = "itms-services://?action=download-manifest&url=https://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/install-manifest.plist" : window.location.href = "http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKEx-android.apk"
-});
