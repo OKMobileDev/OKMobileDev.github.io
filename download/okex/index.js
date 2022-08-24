5cc127075afef111b2903b730b18e40045a32002
OKMobile <okextech@gmail.com>
2022-08-24 10:32:53 +0800

    update url

--- download/okex/index.js
+++ download/okex/index.js
@@ -76,7 +76,7 @@ new Vue({
         }else if(query.indexOf('from=lenovo') != -1) {
             this.androidDownloadURL = 'https://hkpublic.oss-accelerate.aliyuncs.com/upgradeapp/OKEx-android-lenovo.apk'
         } else {
-            this.androidDownloadURL = 'http://okg-pub-hk.oss-cn-hongkong.aliyuncs.com//upgradeapp/okx-android.apk'
+            this.androidDownloadURL = 'https://okg-pub-hk.oss-cn-hongkong.aliyuncs.com//upgradeapp/okx-android.apk'
         }
 
         // 引导至官网
