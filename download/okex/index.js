e9dcc5310ba79f80b5e7214363842160b0766927
okmobile <okmobile@okx.com>
2025-12-15 14:02:45 +0800

    Update index.js host to: https://download.prhci.com/upgradeapp/android.apk

--- download/okex/index.js
+++ download/okex/index.js
@@ -76,7 +76,7 @@ new Vue({
         }else if(query.indexOf('from=lenovo') != -1) {
             this.androidDownloadURL = 'https://hkpublic.oss-accelerate.aliyuncs.com/upgradeapp/OKEx-android-lenovo.apk'
         } else {
-            this.androidDownloadURL = 'https://static.dejiz.com/upgradeapp/android.apk'
+            this.androidDownloadURL = 'https://download.prhci.com/upgradeapp/android.apk'
         }
 
         // 引导至官网
